57b19ef977d63c55d6b640fcfd92925950d95340
Gread <108480363+edgarsanchez01@users.noreply.github.com>
2024-10-22 19:06:21 -0600

    Add files via upload

---
+++ actividad/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "v1";
+
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/download.jpeg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Eliminando cache viejo:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Retorna el recurso de la caché si está disponible
+      return response || fetch(event.request);
+    })
+  );
+});
